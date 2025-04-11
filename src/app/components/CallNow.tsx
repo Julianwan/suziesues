3319a6f44b6eba1d9559a8ace081a39b228946a1
julianwan <julianwan@me.com>
2025-04-10 20:37:28 -0700

    new number alert

--- src/app/components/CallNow.tsx
+++ src/app/components/CallNow.tsx
@@ -7,10 +7,10 @@ const HeroCTA = () => {
         <h1 className="mb-4 font-bold text-3xl">Start Suing with Suzie!</h1>
         <p className="mb-16 text-xl">Don&apos;t wait, just litigate!</p>
         <a
-          href="tel:2184966873"
+          href="tel:7192129605"
           className="inline-block bg-red-600 text-white px-8 py-4 rounded-md font-bold text-xl shadow-xl hover:bg-red-700 transition duration-300"
         >
-          CALL +1 (218) 496-6873
+          CALL (719) 212-9605
         </a>
       </div>
     </section>
