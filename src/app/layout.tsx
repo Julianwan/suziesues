0ae01afcaf818657fb3b0ea8466c8d73cecc4419
julianwan <julianwan@me.com>
2025-04-10 20:01:55 -0700

    apple

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth"><head><link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐁</text></svg>"></link></head>
+    <html lang="en" className="scroll-smooth"><head><link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🍎</text></svg>"></link></head>
       <body className={inter.className}>{children}</body>
     </html>
   );
