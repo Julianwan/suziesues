4c81dce778d37da60ef6e5c9459c35bcac49ee5d
julianwan <julianwan@me.com>
2025-04-10 19:44:21 -0700

    consider this

--- src/app/components/CallNow.tsx
+++ src/app/components/CallNow.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeroCTA = () => {
   return (
-    <section className="bg-gray-100 py-16">
+    <section className="bg-gray-100 dark:bg-red-800 py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <h1 className="mb-4 font-bold text-3xl">Start Suing with Suzie!</h1>
         <p className="mb-16 text-xl">Don&apos;t wait, just litigate!</p>
